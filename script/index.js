cc54b2ee579a18e82c25e459a24c91af0e47b506
Alex Oliveira <alex_oliveira08@yahoo.com.br>
2020-06-10 14:21:33 -0300

    Ajustes no index.html, index,css e index.js

--- script/index.js
+++ script/index.js
@@ -1,3 +1,41 @@
-p.onclick = function (){
-	alert("Testando")
-}
+let mensage = document.getElementById("mensage")
+let button = document.querySelector(".content2 button")
+
+const bra = "https://image.flaticon.com/icons/svg/206/206597.svg"
+const usa = "https://image.flaticon.com/icons/svg/206/206626.svg"
+const fra = "https://image.flaticon.com/icons/svg/206/206657.svg"
+const arg = "https://image.flaticon.com/icons/svg/206/206654.svg"
+const ger = "https://image.flaticon.com/icons/svg/206/206690.svg"
+const jpn = "https://image.flaticon.com/icons/svg/206/206789.svg"
+
+// criando elemento "img" e "p"
+const img = document.createElement("img")
+const p = document.createElement("p")
+
+// mensagens
+mensage.appendChild(img).setAttribute("src",bra)
+mensage.appendChild(p).innerHTML = "Hello"
+
+
+
+
+
+
+
+
+
+
+
+button.addEventListener("click", function () {
+
+	mensage.style.display = "inline"
+	mensage.style.marginTop = "50px"
+
+	setTimeout(function(){
+		mensage.style.marginTop = "-50px"
+	},2000)
+
+})
+
+
+
