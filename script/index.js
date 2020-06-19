a77f11d5a3d4fd882cb94be89bad634b8807015f
Alex Oliveira <alex_oliveira08@yahoo.com.br>
2020-06-19 17:05:56 -0300

    Incluindo mapa

--- script/index.js
+++ script/index.js
@@ -4,38 +4,45 @@ const paises = [
 	{
 		nome: "Brasil",
 		sigla: "bra",
+		cod: "pt",
 		url_img: "https://image.flaticon.com/icons/svg/206/206597.svg",
-		text: "Ola! Seja muito bem-vindo :)"
+		text: "Olá! Seja bem-vindo :)"
 	},
 	{
 		nome: "Estados Unidos",
 		sigla: "usa",
+		cod: "en",
 		url_img: "https://image.flaticon.com/icons/svg/206/206626.svg",
-		text: ""
+		text: "Hello! Welcome :)"
+
 	},
 	{
 		nome: "França",
 		sigla: "fra",
+		cod: "fr",
 		url_img: "https://image.flaticon.com/icons/svg/206/206657.svg",
-		text: ""
+		text: "Bonjour et bienvenue :)"
 	},
 	{
 		nome: "Argentina",
 		sigla: "arg",
+		cod: "es",
 		url_img: "https://image.flaticon.com/icons/svg/206/206654.svg",
-		text: ""
+		text: "¡Hola! Bienvenido :)"
 	},
 	{
 		nome: "Alemanha",
 		sigla: "ger",
+		cod: "de",
 		url_img: "https://image.flaticon.com/icons/svg/206/206690.svg",
-		text: ""
+		text: "Hallo! Willkommen :)"
 	},
 	{
 		nome: "Japão",
 		sigla: "jpn",
+		cod: "ja",
 		url_img: "https://image.flaticon.com/icons/svg/206/206789.svg",
-		text: ""
+		text: "こんにちは！ようこそ :)"
 	}
 ]
 
@@ -51,37 +58,39 @@ const p = document.createElement("p")
 mensage.appendChild(img)
 mensage.appendChild(p)
 
-// Pegando os index's do objeto
- const index = Object.keys(paises)
 
- let rdnindex =  Math.floor(Math.random() * (index.length)) + 0
+// Pegando os index's do objeto
+const index = Object.keys(paises)
+
+let rdnindex = Math.floor(Math.random() * (index.length)) + 0
 
 // console.log("random CEIL: "+ Math.ceil(Math.random() * (index.length)-1))
 // console.log("random FLOOR: " + rdnindex)
 
 
-
 // Evento cLick
 button.addEventListener("click", function () {
 
- // Pegando index radomico
- let rdnindex =  Math.floor(Math.random() * (index.length)) + 0
- // Atribuido index radomico para peger objeto img
- img.setAttribute("src", paises[rdnindex].url_img)
- // Adicionando texto de acordo com a imagem
- p.innerText = paises[rdnindex].text
- 
-// Alterando css
+	// Pegando index radomico
+	let rdnindex = Math.floor(Math.random() * (index.length)) + 0
+
+	// Atribuido index radomico no objeto img
+	img.setAttribute("src", paises[rdnindex].url_img)
+
+	// Adicionando texto de acordo com a imagem
+	p.innerText = paises[rdnindex].text
+
+	// Alterando css
 	mensage.style.display = "inline"
-	mensage.style.marginTop = "0px"
-	img.style.height = "150px"
+	mensage.style.marginTop = "-5px"
+	img.style.height = "180px"
 	p.style.textAlign = "center"
 
 	// Timer para voltar para posição inicial
 	setTimeout(function () {
-		mensage.style.marginTop = "-60px"
+		mensage.style.marginTop = "-80px"
 		img.style.height = "20px"
-	}, 3000)
+	}, 2500)
 
 })
 
---
+++ script/maps.js
@@ -0,0 +1,9 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("maps"), {
+    center: { lat: -14.5485273, lng: -52.8277817 },
+    //-14.5485273,-52.8277817
+    zoom: 3
+  });
+}
+
